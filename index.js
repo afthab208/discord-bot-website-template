713e511260bb11d2c7fc5092f42cd500909b9ee8
Afthab <75113201+afthab208@users.noreply.github.com>
2021-02-14 11:43:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Get all "navbar-burger" elements
+ 
   const $navbarBurgers = Array.prototype.slice.call(
     document.querySelectorAll(".navbar-burger"),
     0
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
  
   if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
+    
     $navbarBurgers.forEach((el) => {
       el.addEventListener("click", () => {
-        // Get the target from the "data-target" attribute
+       
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
